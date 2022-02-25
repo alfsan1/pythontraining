f3d113d94b43ed8c7ee511dbb6f09e9d09610a80
Alfredo Santos <santosabascal@gmail.com>
2022-02-25 09:21:57 -0600

    segundo commit del día, clase de python pero falta que funcione el último bloque de código

--- Skillshare/jsondump_store_data.py
+++ Skillshare/jsondump_store_data.py
@@ -4,5 +4,16 @@
 
 import json 
 
-phone_number = [1234536560]
+phone_numbers = []
+phone_number = int(input("Please write your phonenumber "))
 
+#quiero crear un loop para que los items se guarden en el json file. 
+for item in phone_numbers:
+    phone_numbers.append(item)
+
+
+filename = ("phone_number.json")
+
+with open(filename, "w") as file_object:
+    json.dump(str(phone_numbers), file_object )
+    
