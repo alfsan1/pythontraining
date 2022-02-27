4053a01a68994b1b8a0f5b72c527af58f10b2f57
alfsan1 <santosabascal@gmail.com>
2022-02-27 10:28:12 -0600

    updates from mac

--- Skillshare/add_text_to_file.py
+++ Skillshare/add_text_to_file.py
@@ -9,4 +9,6 @@ def Question():
 
     with open(file_path, "a") as file_object:
         file_object.write(message.title() + "\n")
+
+    print("Thanks for adding " + message.title() + ", it has been registered")
 Question()
